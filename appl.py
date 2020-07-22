77c2317ca8d02a976565f04edf29e0b36bffb645
Muhammad Adi Prasojo <adiprasojo11@gmail.com>
2020-07-22 14:57:13 +0700

    Raise for status.

--- appl.py
+++ appl.py
@@ -166,7 +166,8 @@ def reroute(short_url):
 	url = 'https://nilaiku-rama.microaid.io/profile/' + response['uuid']
 
 	try:
-		shorty_api.shorten_profile(url, short_url.upper())
+		res = shorty_api.shorten_profile(url, short_url.upper())
+		res.raise_for_status()
 	except Exception as error:
 		print(error)
 
